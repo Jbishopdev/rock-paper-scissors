92772895934210237c4af3ace9da9a118f464940
Jbishopdev <osrsironmangim@gmail.com>
2024-11-26 09:31:52 -0500

    Add getHumanChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,21 @@
 function getComputerChoice () {
-  const number = Math.random()
+  let number = Math.random();
   if (number < 1/3) {
     return 'Rock'
   } else if (number >= 1/3 && number < 2/3) {
     return 'Paper'
   } else return 'Scissors'
 }
+
+function getHumanChoice () {
+
+  let choice = "";
+
+  while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+
+    choice = prompt("Please enter rock, paper, or scissors.").toLowerCase();
+  }
+
+}
+
+getHumanChoice();
