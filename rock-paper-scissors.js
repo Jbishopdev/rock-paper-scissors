1784f9fb8d6b1c77ffd0ad7c22b9a22e710ec8de
Jbishopdev <osrsironmangim@gmail.com>
2024-11-26 09:09:46 -0500

    Refactor rock-paper-scissors.js, add getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,39 +1,8 @@
-let computerMove = Math.random();
-let playerMove = "";
-let tie = () => {
-  console.log('You tied!');
-}
-let lose = () => {
-  console.log('You lost!');
-}
-let win = () => {
-  console.log('You won!');
-}
-
-function calculateWinner(computerMove, playerMove){
-  if (computerMove >= 0 && computerMove < 1/3) {
-    if (playerMove === 'Rock') {
-      tie();
-    } else if (playerMove === 'Paper') {
-      win();
-    } else {
-      lose();
-    }
-  } else if (computerMove >= 1/3 && computerMove < 2/3) {
-    if (playerMove === 'Paper') {
-      tie();
-    } else if (playerMove === 'Scissors') {
-      win();
-    } else {
-      lose();
-    }
-  } else {
-    if (playerMove === 'Scissors') {
-      tie();
-    } else if (playerMove === 'Rock') {
-      win();
-    } else {
-      lose();
-    }
-  }
+function getComputerChoice () {
+  const number = Math.random()
+  if (number < 1/3) {
+    return 'Rock'
+  } else if (number >= 1/3 && number < 2/3) {
+    return 'Paper'
+  } else return 'Scissors'
 }
